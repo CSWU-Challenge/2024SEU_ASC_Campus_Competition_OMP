c90813189a7210420868be43b87e0a8a9e3569d8
haorui <60883781+haoruilee@users.noreply.github.com>
2024-12-22 22:53:04 +0800

    Add copyright

--- MatrixOMP.cpp
+++ MatrixOMP.cpp
@@ -1,3 +1,7 @@
+// =========================================================
+// Copyright © 2024 SEU Linux Association ASC Programming Contest Team
+// All rights reserved.
+// =========================================================
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
